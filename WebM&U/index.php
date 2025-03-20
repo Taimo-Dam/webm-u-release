2d2d0723cdfe942c289e94b5f281313a95bd73e4
Taimo-Dam <chickenvn003@gmail.com>
2025-03-20 13:15:39 +0700

    Add files via upload

---
+++ WebM&U/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css.css" />
+<title>Trang chủ M&U</title>
+</head>
+
+<body>
+<div class="header">
+<?php
+    include('modu/header.html');
+    include('modu/main.html');
+	include('discover.html');
+    include('albums.html');
+    include('modu/footer.html');
+?>
+</div>
+
+</body>
+</html>
